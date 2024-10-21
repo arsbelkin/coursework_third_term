ba9ff862dbe7ab9e48c063d4b603b12f560cac82
Arseniy Belkin <arseniibelkin@gmail.com>
2024-10-21 23:02:17 +0300

    edit data.json, turing_machine and several tests

--- turing_machine/tape.py
+++ turing_machine/tape.py
@@ -7,33 +7,38 @@ class DoubleLinkedList:
 
         def __str__(self) -> str:
             return str(self.value)
-    
+
     def __init__(self) -> None:
         self.head = None
         self.tail = None
         self.len = 0
- 
+
     def is_empty(self):
         return not self.__len
-    
+
     def push_front(self, value=None) -> None:
         node = self.Node(value=value)
         node.next = self.head
-        if self.head is not None: self.head.prev = node
-        if self.tail is None: self.tail = node
+        if self.head is not None:
+            self.head.prev = node
+        if self.tail is None:
+            self.tail = node
         self.head = node
         self.len += 1
-    
+
     def push_back(self, value=None) -> None:
         node = self.Node(value=value)
         node.prev = self.tail
-        if self.head is None: self.head = node
-        if self.tail is not None: self.tail.next = node
+        if self.head is None:
+            self.head = node
+        if self.tail is not None:
+            self.tail.next = node
         self.tail = node
         self.len += 1
 
     def pop_front(self) -> Node:
-        if self.head is None: return 
+        if self.head is None:
+            return
 
         node = self.head
         if self.head.next is not None:
@@ -44,9 +49,10 @@ class DoubleLinkedList:
         self.len -= 1
 
         return node
-    
+
     def pop_back(self) -> Node:
-        if self.tail is None: return
+        if self.tail is None:
+            return
 
         node = self.tail
         if self.tail.prev is not None:
@@ -57,7 +63,7 @@ class DoubleLinkedList:
         self.len -= 1
 
         return node
-    
+
     def get_node(self, index: int) -> Node:
         node = self.head
         n = 0
@@ -69,16 +75,18 @@ class DoubleLinkedList:
             n += 1
 
         return node
-    
+
     def __getitem__(self, index: int) -> Node:
         return self.get_node(index)
-    
+
     def insert(self, index: int, value) -> Node:
         right = self.get_node(index)
-        if right is None: return self.push_back(value=value)
+        if right is None:
+            return self.push_back(value=value)
 
         left = right.prev
-        if left is None: return self.push_front(value=value)
+        if left is None:
+            return self.push_front(value=value)
 
         node = self.Node(value=value)
         node.prev = left
@@ -87,14 +95,17 @@ class DoubleLinkedList:
         right.prev = node
 
         return node
-    
+
     def erase(self, index: int) -> Node:
         node = self.get_node(index)
-        if node is None: return
+        if node is None:
+            return
 
-        if node.prev is None: return self.pop_front()
-        
-        if node.next is None: return self.pop_back()
+        if node.prev is None:
+            return self.pop_front()
+
+        if node.next is None:
+            return self.pop_back()
 
         left = node.prev
         right = node.next
@@ -102,16 +113,18 @@ class DoubleLinkedList:
         right.prev = left
 
         return node
-    
+
     def clear(self) -> None:
         while self.head is not None:
             self.pop_front()
 
     def print_list(self) -> None:
         current_node = self.head
-        
+
         while current_node:
-            print(current_node.value if current_node.value is not None else "_", end=" ")
+            print(
+                current_node.value if current_node.value is not None else "_", end=" "
+            )
             current_node = current_node.next
         print()
 
@@ -119,7 +132,8 @@ class DoubleLinkedList:
         current_node = self.head
         result = ""
         while current_node:
-            result += (current_node.value if current_node.value is not None else "_") + " "
+            result += (
+                current_node.value if current_node.value is not None else "_"
+            ) + " "
             current_node = current_node.next
         return result
-    
--- turing_machine/turing_machine.py
+++ turing_machine/turing_machine.py
@@ -8,34 +8,188 @@ class TuringMachine:
         self.data = self.__load_data(path_to_file)  # данные
         self.alphabet = self.data["alphabet"]  # алфавит
         self.word = self.data["word"]  # слово
-        self.start_condition = self.data["start_condition"]  # стартовое состояние
-        self.start_pointer = self.data[ "start_pointer"]  # стартовая позиция
-        self.conditions = self.data["conditions"]  # состояния
+        self.condition = self.data[
+            "start_condition"
+        ]  # состояние(при инициализации стартовое состояние)
+        self.pointer = self.data[
+            "start_pointer"
+        ]  # позиция курсора(при инициализации стартовое положение)
+        self.conditions = self.data["conditions"]  # массив состояний и инструкций
 
-        self.__set_word(self.word)
-    
+        self.__set_word(self.word)  # запись слова в ленту
+
+        # TODO: удалить
+        self.__min = 0
+        self.__max = len(self.word)
+
     def __load_data(self, path_to_file) -> dict:
         "функция для загрузки данных из файла"
         with open(path_to_file) as file:
             data = json.load(file)
         return data
-    
+
     def __set_word(self, word) -> None:
-        self.tape.push_front()
+        # self.tape.push_front()
         for elem in word:
             self.tape.push_back(elem)
-        self.tape.push_back()
+        # self.tape.push_back()
 
     def print_tape(self):
-        pos = str(self.tape).find(self.word[self.start_pointer])
+        pos = str(self.tape).find(self.tape[self.pointer].value)
         self.tape.print_list()
-        print(" "*pos + "x")
+        print(" " * pos + "x")
 
+    def next_step(self) -> None:
+        condition = self.conditions[int(self.condition[1::])]
+        # symbol = (
+        #     self.tape[self.pointer].value if 0 <= self.pointer < len(self.word) else "empty"
+        # )
+        symbol = self.tape[self.pointer].value
+        command = condition["commands"][symbol].split()
 
+        if len(command) == 1:
+            if command[0] == "!":
+                self.tape.print_list()
+                return False
+            else:
+                if command[0] == "L":
+                    self.pointer -= 1
+                    if self.pointer < 0:
+                        self.tape.push_front("empty")
+                        self.pointer = 0
+                elif command[0] == "R":
+                    self.pointer += 1
+                    if self.pointer > self.tape.len - 1:
+                        self.tape.push_back("empty")
+                        self.pointer -= 1
+        else:
+            new_symbol, new_pointer, new_condition = command
 
+            if new_symbol != "_":
+                self.tape[self.pointer].value = new_symbol
 
+            if new_pointer == "L":
+                self.pointer -= 1
+                if self.pointer < 0:
+                    self.tape.push_front("empty")
+                    self.pointer = 0
+            elif new_pointer == "R":
+                self.pointer += 1
+                if self.pointer > self.tape.len - 1:
+                    self.tape.push_back("empty")
+                    self.pointer -= 1
+
+            if new_condition != "!":
+                self.condition = new_condition
+            else:
+                self.condition = self.data[
+            "start_condition"
+        ] 
+                self.tape.print_list()
+                return False
+        
+        return True
+            
+    def run(self):
+        flag = True
+        while flag:
+            flag = self.next_step()
+        #print(self.tape.print_list())
 
 
 tm = TuringMachine()
 
-tm.print_tape()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+tm.run()
+
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
+
+# tm.next_step()
+# print(tm.pointer)
+# print(tm.condition)
+# # tm.print_tape()
+# tm.tape.print_list()
